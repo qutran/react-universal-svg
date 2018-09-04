b1af39c29127ee79b935b5a056ace591ac106558
dmitrykutran <dkutran@jssolutions.com>
2018-09-04 12:52:10 +0300

    Platform and env code splitting

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
-import DevBase from './starters/dev.base';
+import { Platform } from 'react-primitives'
 
 export default (() => {
+  if (process.env.NODE_ENV === 'production') {
+    if (['ios', 'android'].indexOf(Platform.OS) >= 0) {
+      return require('./svg.native').default;
+    }
+
+    return require('./svg.web').default;
+  }
+
   if (process.env.REACT_APP_ENV === 'development:dom') {
     return import('./starters/dev.dom');
   }
 
   if (process.env.REACT_APP_ENV === 'development:native') {
+    const DevBase = require('./starters/dev.base').default;
     return () => <DevBase loader={import('./starters/dev.native')} />;
   }
 })();
