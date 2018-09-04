93db6a17e395647ac3229eabb56d66589b9e1e88
dmitrykutran <dkutran@jssolutions.com>
2018-09-04 20:56:52 +0300

    Add type definitions

---
+++ index.d.ts
@@ -0,0 +1,40 @@
+import * as React from 'react';
+
+export interface onDimensionsParams {
+  width: number;
+  height: number;
+}
+
+export interface SvgProps {
+  /**
+   * Svg source. 
+   */
+  src: string;
+  /**
+   * Svg style.
+   */
+  style: any;
+  /**
+   * Params injectable into svg template. Ex. { color: 'red' } will put value 'red' where ${color} string presents.
+   */
+  params: any;
+  /**
+   * onDimensions callback will trigger with computed real width and height of svg.
+   */
+  onDimensions: (params: onDimensionsParams) => null;
+}
+
+export interface ModulesSignature {
+  [key: string]: any;
+}
+
+/**
+ * Universal Svg component
+ */
+export default class svg extends React.Component<SvgProps> {
+  /**
+   * Function for local svg modules registry. Ex. addModules({ mySvg: require('assets/mySvg') }). After that usse Svg component's prop src like src="mySvg".
+   * @param modules Local svg modules registry.
+   */ 
+  static addModules(modules: ModulesSignature): null;
+}
